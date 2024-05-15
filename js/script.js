d2c307bbc5247d9a4229c577aeca5262437f7c77
LearnCodingEasy <learncodingeasy@yahoo.com>
2024-05-15 21:21:01 +0300

    bulmaToast

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 function copyImagePath(element) {
   // الحصول على الصورة القريبة من الزر الذي تم النقر عليه
-  var imagePath = element.previousElementSibling.getAttribute('src');
+  var imagePath = element.previousElementSibling.getAttribute("src");
 
   // إنشاء عنصر input من النوع text
-  var input = document.createElement('input');
-  input.setAttribute('value', imagePath);
+  var input = document.createElement("input");
+  input.setAttribute("value", `https://learncodingeasy.github.io/Images/${imagePath}`);
   document.body.appendChild(input);
 
   // تحديد نص مسار الصورة
   input.select();
 
   // نسخ النص إلى الحافظة
-  document.execCommand('copy');
+  document.execCommand("copy");
 
   // إزالة العنصر input
   document.body.removeChild(input);
 
-  // إظهار رسالة تأكيد النسخ
-  alert('تم نسخ مسار الصورة بنجاح: ' + imagePath);
+  // عرض رسالة الإشعار باستخدام Bulma Toast
+  bulmaToast.toast({
+    message: "تم نسخ مسار الصورة بنجاح",
+    type: "is-success",
+    dismissible: true,
+    pauseOnHover: true,
+    duration: 2000,
+    position: "bottom-right"
+  });
 }
